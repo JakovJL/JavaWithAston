fb7c9ad91cfd279e1c96b1342b94c73ce32c0376
JakovJL <mr.nalegach@gmail.com>
2025-04-11 15:50:01 +0300

    Added helicopter class and its instance

--- src/main/java/Module_1n2/HierarchyOfVehicle.java
+++ src/main/java/Module_1n2/HierarchyOfVehicle.java
@@ -25,6 +25,7 @@ interface HasCargo {
      Wheel(int wheelCount) {
          this.wheelCount = wheelCount;
      }
+
     public   int getWheelCount() {
          return wheelCount;
      }
@@ -65,20 +66,40 @@ class Wing{
 
 public class HierarchyOfVehicle {
     public static void main(String[] args) {
-
-        System.out.println("_____________GO__________");
+        // самолет
         Wing[] wings = { new Wing("Swept", 60),
                          new Wing("Swept", 60) };
+
         Cargo cargo = new Cargo(5000);
-        Wheel[] wheels = { new Wheel(1),
-                           new Wheel(2),
+
+        Wheel[] wheels = { new Wheel(2), // переднее сдвоенное
+                           new Wheel(2), // задние сдвоенные  (справа, слева)
                            new Wheel(2) };
+
         Propeller[] propellers = { new Propeller("Turbofan"),
                                    new Propeller("Turbofan") };
 
         Airplane airplane = new Airplane("White",5000,3_000_000,"Boeing 747",
                 15_000,20_000,wings, cargo, wheels, propellers);
         System.out.println(airplane);
+        airplane.launchEngine();
+        airplane.stopEngine();
+
+        //Вертолет
+        Cargo heliCargo = new Cargo(2000);
+
+
+        Wheel[] heliWheels = { new Wheel(1),
+                               new Wheel(1),
+                               new Wheel(1) };
+        Propeller[] heliPropellers = { new Propeller("Main rotor"),
+                                       new Propeller("Tail rotor") };
+
+        Helicopter helicopter = new Helicopter("Green", 2000, 5000, "Mi-8",
+                5500, 750, heliCargo, heliPropellers, heliWheels);
+        System.out.println("\n" + helicopter);
+        helicopter.launchEngine();
+        helicopter.stopEngine();
 
 
     }
@@ -108,6 +129,7 @@ abstract class Vehicle {
     public int getWeight() {
         return weight;
     }
+
     public abstract void launchEngine();
     public abstract void stopEngine();
 
@@ -184,47 +206,78 @@ class Airplane extends Vehicle implements HasWheels, HasPropeller, HasWings, Has
     @Override
     public String toString() {
         return String.format(
-                "Airplane [Model: %s | Altitude: %,d m | Range: %,d km | " +
-                        "Wings: %d | Wheels: %d | Propellers: %d | Cargo: %,d kg]",
+                "Airplane \n[Model: %s | Altitude: %,d m | Range: %,d km | " +
+                        "Wings: %d | Wheels: %d | Propellers: %d | Cargo: %,d kg]" +
+                        "\n[Color: %s | Horse power: %d] ",
                 model, maxFlightAltitude, maxDistanceRange,
-                wings.length, wheels.length, propellers.length, cargo.getCargoWeight()
+                wings.length, wheels.length, propellers.length, cargo.getCargoWeight(),
+                this.getColor(), this.getHorsePower()
         );
     }
 }
-//    class Helicopter extends Vehicle implements HasWheels, HasPropeller, HasCargo {
-//    private final String model;
-//    private final int maxFlightAltitude;
-//    private final int maxDistanceRange;
-//
-//
-//    public Helicopter(String color, int horsePower, int weight, String model,
-//                      int maxFlightAltitude, int maxDistanceRange) {
-//        super(color, horsePower, weight);
-//        this.model = model;
-//        this.maxFlightAltitude = maxFlightAltitude;
-//        this.maxDistanceRange = maxDistanceRange;
-//    }
-//    public Helicopter(){
-//        super("White",3500,20_000);
-//        this.model = "Boeing 747";
-//        this.maxFlightAltitude = 10_000;
-//        this.maxDistanceRange = 15_000;
-//
-//    }
-//// методы интефейсов
-//
-////методы родителя
-//    @Override
-//    public void launchEngine() {
-//        System.out.println("Launch airplane Engine!");
-//    }
-//
-//    @Override
-//    public void stopEngine() {
-//        System.out.println("Stop airplane Engine!");
-//    }
-//
-//}
+    class Helicopter extends Vehicle implements HasWheels, HasPropeller, HasCargo {
+    private final String model;
+    private final int maxFlightAltitude;
+    private final int maxDistanceRange;
+        private final Cargo cargo;
+        private final Propeller[] propellers;
+        private final Wheel[] wheels;
+
+
+        public Helicopter(String color, int horsePower, int weight, String model, int maxFlightAltitude,
+                          int maxDistanceRange, Cargo cargo, Propeller[] propellers, Wheel[] wheels) {
+            super(color, horsePower, weight);
+            this.model = model;
+            this.maxFlightAltitude = maxFlightAltitude;
+            this.maxDistanceRange = maxDistanceRange;
+            this.cargo = cargo;
+            this.propellers = propellers;
+            this.wheels = wheels;
+        }
+
+        //методы родителя
+    @Override
+    public void launchEngine() {
+        System.out.println("Launch helicopter Engine!");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("Stop helicopter Engine!");
+    }
+
+    // методы интерфейса
+
+        @Override
+        public Cargo getCargo() {
+            return this.cargo;
+        }
+
+
+        @Override
+        public Propeller[] getPropellers() {
+            return this.propellers;
+        }
+
+        @Override
+        public Wheel[] getWheels() {
+            return this.wheels;
+        }
+
+
+        @Override
+        public String toString() {
+            return String.format(
+                    "Helicopter \n[Model: %s | Altitude: %,d m | Range: %,d km | " +
+                            "Wheels: %d | Propellers: %d | Cargo: %,d kg]" +
+                            "\n[Color: %s | Horse power: %,d]",
+                    model, maxFlightAltitude, maxDistanceRange,
+                    wheels.length, propellers.length, cargo.getCargoWeight(),
+                    this.getColor(), this.getHorsePower()
+            );
+        }
+
+    }
 
 
 
